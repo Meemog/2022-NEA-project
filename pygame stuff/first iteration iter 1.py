4225c2251c8275c134399fc628c1578aaf570e66
Armin Raad <armin3600z@gmail.com>
2021-10-11 13:36:47 +0100

    made so that the letter stops moving accross the screen when it is in the middle, deleting will bring previous letters back

--- pygame stuff/first iteration iter 1.py
+++ pygame stuff/first iteration iter 1.py
@@ -29,20 +29,24 @@ boxX = dispWidth / 5
 boxY = 6 * dispHeight / 20
 #defines the box position
 boxCoords = (boxX, boxY)
-boxColour = boxColourDormant
 #defines box colour
-box = pygame.Rect(boxCoords, (boxWidth, boxHeight))
+boxColour = boxColourDormant
 #defines box in format of a pygame.rect()
-text = ''
+box = pygame.Rect(boxCoords, (boxWidth, boxHeight))
 #sets text string to empty so it can be added to later
-font = pygame.font.Font(None, fontSize)
+text = ''
+#removedText will be a list that is treated as a stack, removing from the end and adding to the end
+removedText = []
 #sets the font to the default with size 32
+font = pygame.font.Font(None, fontSize)
 #time between backspaces
 timeBetweenBackspaces = 50
 #time since last backspace
 timeSinceLastBackspace = 0
 #boolean for whether or not the current text is being deleted
 deleting = False
+#typing boolean to determine if the textbox is focused
+typing = False
 
 #experimental bits
 pygame.key.start_text_input()
@@ -74,6 +78,7 @@ while GAMELOOP:
             if event.key == pygame.K_RETURN:
                 print(text) #does stuff with the text that was written
                 text = ''   #resets the text to empty string
+                removedText = []
             #detects backspace being pressed down
             elif event.key == pygame.K_BACKSPACE:
                 text = text[:-1] #removes 1 letter from the end of the text
@@ -81,20 +86,36 @@ while GAMELOOP:
                 timeSinceLastBackspace = 0
             else:
                 text += event.unicode
+
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_BACKSPACE:
                 deleting = False
+                #brings back a letter that was removed if the string was too long
+                if len(removedText) > 0:
+                    text = removedText.pop() + text
+
     #deletes text while the key is held down        
-    if deleting and timeSinceLastBackspace > timeBetweenBackspaces and typing:                    
+    if deleting and timeSinceLastBackspace > timeBetweenBackspaces and typing:                
         text = text[:-1]
         timeSinceLastBackspace = 0
+        #brings a letter back from the string
+        if len(removedText) > 0:
+            text = removedText.pop() + text
 
+    #gets the time since thelast frame in milliseconds
     timeSinceLastBackspace += gameClock.get_time() 
 
-    window.fill(backColour)
+    #removes a character at the start when text is too long
+    if font.size(text)[0] > boxWidth / 2:
+        removedText.append(text[0])
+        text = text[1:] 
+
     textRender = font.render(text, True, textColour)
+    #draws rectangle (textbox)
+
+    window.fill(backColour)
     pygame.draw.rect(window, boxColour, box)
-    #draws a rectangle with colour boxColour
+    #draws the text over the rectangle
     window.blit(textRender, (boxX+5, boxY+5))
 
     pygame.display.update()
