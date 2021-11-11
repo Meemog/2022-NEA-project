d1218902c1acb505eed5a40643567b9487d5f7d7
Armin Raad <armin3600z@gmail.com>
2021-11-11 13:22:40 +0000

    client.py is now a client object

--- client.py
+++ client.py
@@ -1,37 +1,39 @@
 import socket
 import threading
 
-HEADER = 8
-PORT = 5000
-SERVER = socket.gethostbyname(socket.gethostname()) #temporary // sets ip of host to client ip, which is same as host ip
-FORMAT = 'utf-8'
-ADDRESS = (SERVER, PORT)
+class Client:
+    def __init__(self):
+        self.__HEADER = 8
+        self.__PORT = 5000
+        self.__SERVER = socket.gethostbyname(socket.gethostname()) #temporary // sets ip of host to client ip, which is same as host ip
+        self.__FORMAT = 'utf-8'
+        self.__ADDRESS = (self.__SERVER, self.__PORT)
+        self.__client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Same as before, ipv4 and TCP
+        self.__client.connect(self.__ADDRESS) #Connects to the right local address, in this case its my own pc
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Same as before, ipv4 and TCP
-client.connect(ADDRESS) #Connects to the right local address, in this case its my own pc
+    def SendMsg(self, msg):
+        encMessage = msg.encode(self.__FORMAT) #encodes msg with utf-8
+        msgLen = len(encMessage)
+        msgLen = str(msgLen).encode(self.__FORMAT) 
+        msgLen += b' ' * (self.__HEADER - len(msgLen)) #makes the message length be 8 bytes long so the server recognises it
+        #b' ' means the byte representation of a space
+        self.__client.send(msgLen)
+        self.__client.send(encMessage)
 
-def SendMsg(msg):
-    encMessage = msg.encode(FORMAT) #encodes msg with utf-8
-    msgLen = len(encMessage)
-    msgLen = str(msgLen).encode(FORMAT) 
-    msgLen += b' ' * (HEADER - len(msgLen)) #makes the message length be 8 bytes long so the server recognises it
-    #b' ' means the byte representation of a space
-    client.send(msgLen)
-    client.send(encMessage)
+    def GetMsgs(self):
+        while True:
+            msgLen = self.__client.recv(self.__HEADER).decode(self.__FORMAT) #Waits for message with length 8 bytes to be received from the client and then decodes it 
+            if msgLen:  #First message will always be empty
+                msgLen = int(msgLen)
+                msg = self.__client.recv(msgLen).decode(self.__FORMAT) #Waits for a message with length msgLen to be received
+                if msg != "":
+                    print(f"\nMessage from server: {str(msg)}")
 
-def GetMsgs():
-    while True:
-        msgLen = client.recv(HEADER).decode(FORMAT) #Waits for message with length 8 bytes to be received from the client and then decodes it 
-        if msgLen:  #First message will always be empty
-            msgLen = int(msgLen)
-            msg = client.recv(msgLen).decode(FORMAT) #Waits for a message with length msgLen to be received
-            if msg != "":
-                print(f"\nMessage from server: {str(msg)}")
-
-receiveThread = threading.Thread(target=GetMsgs)
+client = Client()
+receiveThread = threading.Thread(target=client.GetMsgs)
 receiveThread.start()
 
 newMsg = "hello"
 while True:
-    SendMsg(newMsg)
+    client.SendMsg(newMsg)
     newMsg = input("Client message to server:")
