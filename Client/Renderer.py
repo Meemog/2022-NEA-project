436ea79cbe4a4110f054bbd68209660bf1a51389
Armin <armin3600z@gmail.com>
2021-12-10 07:47:13 +0000

    This broke somehow, commit to fix it

--- Client/Renderer.py
+++ Client/Renderer.py
@@ -4,6 +4,7 @@ class Renderer:
     def __init__(self):
         self.__backColour = (10,10,10)
     
+    #Draws everything on screen
     def Render(self, window, textBox):
         window.fill(self.__backColour)
         pygame.draw.rect(window, textBox.boxColour, textBox.box)
