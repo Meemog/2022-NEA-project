533ed76d5a77dce763b1ff70dfd06cc714982f19
Armin <armin3600z@gmail.com>
2022-03-19 13:52:08 +0000

    Commented InputBox

--- Client/InputBox.py
+++ Client/InputBox.py
@@ -1,18 +1,26 @@
 import pygame
 
 class InputBox:
+    #Does not support changing the size of the rectangle
     def __init__(self, textColour, colourActive, colourDormant, hashedOut, rectangle, dispHeight):
+        #This is where the text that needs to be displayed is stored
         self.text = ""
         self.__textColour = textColour
+        #The box's colour, originally set to inactive
         self.__colour = colourDormant
+        #Determines whether or not the box is selected
         self.isActive = False
+        #Colour for when the box is selected or unselected
         self.__colourActive = colourActive
         self.__colourDormant = colourDormant
+        #Determines if the text should be displayed
         self.__hashedOut = hashedOut
+        #Used for some calculations
         self.__size = rectangle.size
         self.__location = (rectangle.x, rectangle.y)
         self.rectangle = rectangle
 
+        #Finds the largest font size that can fit in the box, given its size
         fontSize = 999
         font = pygame.font.SysFont("Courier New", int(dispHeight * fontSize/1080))
         fontRenderSize = font.size(self.text)
@@ -22,25 +30,32 @@ class InputBox:
             font = pygame.font.SysFont("Courier New", int(dispHeight * fontSize/1080))
             fontRenderSize = font.size(self.text)
 
+        #Font object with correct size
         self.__font = font
 
+    #Renders the box on window
     def DrawBox(self, window):
+        #Draws the rectangle with the right colour
         pygame.draw.rect(window, self.__colour, self.rectangle)
 
+        #Converts text to be starred out if it is meant to be
         if self.__hashedOut:
             text = "*"*len(self.text)
 
         else:
             text = self.text
 
+        #Finds how many letters it needs to cut off from the front for the text to fit in the box
         lettersFromBack = len(text)
         #While the rendered version of text with lettersFromBack number of letters from the back is bigger than the textbox
         while self.__font.size(text[-lettersFromBack:])[0] > self.__size[0] - 10:
             lettersFromBack -= 1
 
+        #Renders the text but cuts off the right amount from the back
         textRender = self.__font.render(text[-lettersFromBack:], True, self.__textColour)
         window.blit(textRender, (self.__location[0] + 5, self.__location[1] + 5))
-        
+    
+    #Checks if location is on the box
     def CheckIfClicked(self, location):
         if self.rectangle.collidepoint(location):
             return True
@@ -48,9 +63,11 @@ class InputBox:
         else:
             return False
 
+    #Adds letter to the text
     def AddLetter(self, letter):
         self.text += letter 
 
+    #Removes a letter if the text is not nothing, removes a word if control is true    
     def RemoveLetter(self, control):
         if self.text != "":
             if control:
@@ -66,13 +83,19 @@ class InputBox:
             else:
                 self.text = self.text[:-1]
 
+    #Sets colour to match the selection status of the box
     def SetActive(self):
         self.__colour = self.__colourActive
         self.isActive = True
 
+    #Sets colour to match the selection status of the box
     def SetDormant(self):
         self.__colour = self.__colourDormant
         self.isActive = False
+
+#--------------------------------
+#This stuff can be ignored, was used for testing and is only still here in case I need it later.
+#--------------------------------
 
 # dispWidth = 1920
 # dispHeight = 1080
