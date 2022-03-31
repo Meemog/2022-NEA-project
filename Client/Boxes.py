c3c01dabd346126f42cb5ba8320480240c46bc85
Armin <armin3600z@gmail.com>
2022-03-31 16:00:58 +0100

    Made variables protected as required

--- Client/Boxes.py
+++ Client/Boxes.py
@@ -3,81 +3,83 @@ import pygame
 #A class box that is used for textboxes and inputboxes
 class Box:
     def __init__(self, rect, font, resolution, colourActive, colourInactive, textColour, text) -> None:
-        self.rect = rect
-        self.font = font
-        self.resolution = resolution
-        self.activeColour = colourActive
-        self.inactiveColour = colourInactive
+        self._rect = rect
+        self._font = font
+        self._resolution = resolution
+        self._activeColour = colourActive
+        self._inactiveColour = colourInactive
         #Colour starts inactive
-        self.colour = colourInactive
         self.isActive = False
+        self._colour = colourInactive
         
-        self.text = text
-        self.textColour = textColour
+        self._text = text
+        self._textColour = textColour
 
     def Render(self, window):
-        pass
+        pygame.draw.rect(window, self._colour, self._rect)
 
     def AddLetter(self, letter):
-        self.text += letter
+        self._text += letter
 
     def RemoveLetter(self, control):
-        if self.text != "":
+        if self._text != "":
             if control:
                 #Removes trailing spaces 
-                while self.text[-1] == " ":
-                    self.text = self.text[:-1]
+                while self._text[-1] == " ":
+                    self._text = self._text[:-1]
                 #Removes letters until a space is reached or text has run out
-                while self.text != "" and self.text[-1] != " ":
-                    self.text = self.text[:-1]
+                while self._text != "" and self._text[-1] != " ":
+                    self._text = self._text[:-1]
 
             else:
-                self.text = self.text[:-1]
+                self._text = self._text[:-1]
 
     def SetActive(self):
         self.isActive = True
-        self.colour = self.activeColour
+        self._colour = self._activeColour
 
     def SetInactive(self):
         self.isActive = False
-        self.colour = self.inactiveColour
+        self._colour = self._inactiveColour
 
     def CheckForCollisionWithMouse(self, mouseLocation):
-        if self.rect.collidepoint(mouseLocation):
+        if self._rect.collidepoint(mouseLocation):
             return True
         return False
 
 class TextBox(Box):
     def __init__(self, rect, font, resolution, colourActive, colourInactive, textColour, text, previewText, previewTextColour, incorrectTextColour) -> None:
         super().__init__(rect, font, resolution, colourActive, colourInactive, textColour, text)
-        self.previewText = previewText
-        self.previewTextColour = previewTextColour
-        self.incorrectTextColour = incorrectTextColour
+        self._previewText = previewText
+        self._previewTextColour = previewTextColour
+        self._incorrectTextColour = incorrectTextColour
 
     def CheckIfFinished(self):
-        if len(self.text) >= len(self.previewText):
+        if len(self._text) >= len(self._previewText):
             return True
         return False
 
-    def Render(self):
+    def Render(self, window):
+        super().Render(window)
+        
         if self.CheckIfFinished():
             return 0
 
         #Length of text is used a lot, this saves some function calls
-        lenText = len(self.text)
+        lenText = len(self._text)
         #Makes text the same length as 
-        text = self.text
-        while self.font.size(text) > (self.rect.width - 10 * self.resolution[0]) / 2:
+        text = self._text
+        while self._font.size(text) > (self._rect.width - 10 * self._resolution[0]) / 2:
             text = text[1:]
 
         #If the preview text isnt empty (Game has started) then it will cut it so that it fits in the textbox
-        if self.previewText != "":
+        if self._previewText != "":
             #Removes same number of letters from start of previewtext as was removed from text
-            previewText = self.previewText
-            previewText = previewText[len(self.text) - len(text):]
+            previewText = self._previewText
+            previewText = previewText[len(self._text) - len(text):]
 
             #Makes sure that previewText doesn't go out the box
-            while self.font.size(previewText) > self.rect.width - 10:
+            while self._font.size(previewText) > self._rect.width - 10:
                 previewText = previewText[:1]
 
             #Replaces correct letters player types with spaces 
@@ -95,15 +97,36 @@ class TextBox(Box):
             cutCorrectText = "".join(cutCorrectText)
 
             #Makes render for preview text and correctly typed text
-            self.previewTextRender = self.font.render(cutPreviewText, True, self.previewTextColour)
-            self.cutCorrectTextRender = self.font.render(cutCorrectText, True, self.textColour)
+            self._previewTextRender = self._font.render(cutPreviewText, True, self._previewTextColour)
+            self.cutCorrectTextRender = self._font.render(cutCorrectText, True, self._textColour)
 
             #Renders incorrectly typed text
-            self.cutIncorrectTextRender = self.font.render(cutText, True, self.wrongTextColour)
+            self.cutIncorrectTextRender = self._font.render(cutText, True, self._incorrectTextColour)
 
-        elif self.previewText == "":
-            self.textRender = self.font.render(cutText, True, self.previewTextColour)
+        elif self._previewText == "":
+            self._textRender = self._font.render(cutText, True, self._previewTextColour)
 
 class InputBox(Box):
-    def __init__(self, rect, colourActive, colourInactive, text=""):
-        super().__init__(rect, colourActive, colourInactive, text)
+    def __init__(self, rect, font, resolution, colourActive, colourInactive, textColour, text, hashed = False) -> None:
+        super().__init__(rect, font, resolution, colourActive, colourInactive, textColour, text)
+        self.__hashed = hashed
+
+    def Render(self, window):
+        super().Render(window)
+
+        if self.__hashed:
+            #Makes hashed string
+            textToRender = len(self._text) * "*"
+        else:
+            textToRender = self._text
+
+        textSize = self._font.size(textToRender)
+
+        #Makes text correct size
+        while textSize > self._rect.width - 10:
+            textToRender = textToRender[1:]
+            textSize = self._font.size(textToRender)
+
+        textRender = self._font.render(textRender, True, self._textColour)
+        textLocation = (self._rect.left + 5 * self._resolution[0], self._rect.top + 5 * self._resolution[1])
+        window.blit(textRender, textLocation)
