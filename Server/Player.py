4436c3fd6c0d3eaf6058a62b68d6bdb65c7ba7fb
Armin <armin3600z@gmail.com>
2022-04-04 13:26:50 +0100

    Added attributes for stats

--- Server/Player.py
+++ Server/Player.py
@@ -10,6 +10,16 @@ class Player:
         self.loggedIn = False
 
         self.username = ""
+        self.wordsTyped = 0
+        self.timePlayed = 0
+        self.Elo = 0
+        self.highestElo = 0
+        self.gamesWon = 0
+        self.gamesPlayed = 0
+        self.longestStreak = 0
+        self.largestWinMargin = 0
+        self.lettersTyped = 0
+        self.lettersTypedCorrectly = 0
 
         self.textWritten = ""
 
